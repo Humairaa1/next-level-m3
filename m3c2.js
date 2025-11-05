a410ef0e365dc3c5bcb60f6468e5b5071f3f5e1c
Humaira <humaira71bd@gmail.com>
2025-11-05 10:57:54 -0800

    first commit

---
+++ m3c2.js
@@ -0,0 +1,20 @@
+class Counter{
+    constructor(count){
+        this.count = count;
+    }
+
+    add(amount){
+        this.count = this.count + amount;
+    }
+
+    print(){
+        console.log(this.count);
+    }
+}
+
+const counter1 = new Counter(10);
+
+counter1.add(2)
+counter1.add(3)
+
+counter1.print();
